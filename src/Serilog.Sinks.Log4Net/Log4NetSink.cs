ec469c9a21b07ec1d9977ea984dc09ff17e65e0d
Matt Davidson <matt.davidson@forceindiaf1.com>
2019-03-27 11:45:17 +0000

    Updates to use latest versions of Serilog, Log4Net and moves to netstandard2.0

--- src/Serilog.Sinks.Log4Net/Log4NetSink.cs
+++ src/Serilog.Sinks.Log4Net/Log4NetSink.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2014 Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using System;
+using System.Reflection;
 using System.Security.AccessControl;
 using Serilog.Core;
 using Serilog.Debugging;
@@ -55,9 +56,8 @@ namespace Serilog.Sinks.Log4Net
 
             var message = logEvent.RenderMessage(_formatProvider);
             var exception = logEvent.Exception;
+            var logger = LogManager.GetLogger(Assembly.GetCallingAssembly(), loggerName);
 
-            var logger = LogManager.GetLogger(loggerName);
-            
             using (_supplyContextMessage ? ThreadContext.Stacks["NDC"].Push(ContextMessage) : new NullDisposable())
             {
                 switch (logEvent.Level)
